99944c904fb2be18e908b7a866d528bc862d5c99
“Alaa <1074002@hr.nl>
2025-03-25 15:25:57 +0100

    Implemented keyboard and mouse input handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, constants, math
-
+import keyboard, mouse
 from pygame import gfxdraw
 
 from img import images
@@ -7,6 +7,7 @@ from img import images
 
 import os
 from win32api import GetSystemMetrics
+
 windowX = GetSystemMetrics(0)/2 - constants.gameW/2
 windowY = GetSystemMetrics(1)/2 - constants.gameH/2
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (windowX, windowY)
@@ -14,16 +15,29 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (windowX, windowY)
 pygame.init()
 from pygame.locals import NOFRAME, DOUBLEBUF
 ctx = pygame.display.set_mode((constants.gameW,constants.gameH), DOUBLEBUF)
+
+
 ctx.set_alpha(None)
 pygame.display.set_caption("Pinball")
 clock = pygame.time.Clock()
-running = True
-while running:
+
+
+def listen(running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            print(f"KEYDOWN: {event.key}")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print(f"MOUSE CLICK: {event.button}")
+        keyboard.listen(event)  # Your custom logic
+        mouse.listen()          # Your custom logic
+    return running
 
-    ctx.fill((0, 0, 0))
+running = True
+while running:
+    running = listen(running)
+    ctx.fill((50, 50, 50))
     pygame.display.flip()
     clock.tick(60)
 
