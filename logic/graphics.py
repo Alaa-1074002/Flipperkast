d6c06a10a077e774080845d8b756a11937d0d0c1
“Alaa <1074002@hr.nl>
2025-03-28 19:14:36 +0100

    Added collision and graphics modules

---
+++ logic/graphics.py
@@ -0,0 +1,53 @@
+def sortByX(a):
+    return a[0]
+
+def sortByY(a):
+    return a[1]
+
+
+
+
+import pygame as pg
+
+from pygame import gfxdraw
+
+
+def round_rect(surface, rect, color, rad=20, border=0, inside=(0,0,0,0)):
+    rect = pg.Rect(rect)
+    zeroed_rect = rect.copy()
+    zeroed_rect.topleft = 0,0
+    image = pg.Surface(rect.size).convert_alpha()
+    image.fill((0,0,0,0))
+    _render_region(image, zeroed_rect, color, rad)
+    if border:
+        zeroed_rect.inflate_ip(-2*border, -2*border)
+        _render_region(image, zeroed_rect, inside, rad)
+    surface.blit(image, rect)
+
+
+def _render_region(image, rect, color, rad):
+    """Helper function for round_rect."""
+    corners = rect.inflate(-2*rad, -2*rad)
+    for attribute in ("topleft", "topright", "bottomleft", "bottomright"):
+        pg.draw.circle(image, color, getattr(corners,attribute), rad)
+    image.fill(color, rect.inflate(-2*rad,0))
+    image.fill(color, rect.inflate(0,-2*rad))
+
+
+def aa_round_rect(surface, rect, color, rad=20, border=0, inside=(0,0,0)):
+    rect = pg.Rect(rect)
+    _aa_render_region(surface, rect, color, rad)
+    if border:
+        rect.inflate_ip(-2*border, -2*border)
+        _aa_render_region(surface, rect, inside, rad)
+
+
+def _aa_render_region(image, rect, color, rad):
+    """Helper function for aa_round_rect."""
+    corners = rect.inflate(-2*rad-1, -2*rad-1)
+    for attribute in ("topleft", "topright", "bottomleft", "bottomright"):
+        x, y = getattr(corners, attribute)
+        gfxdraw.aacircle(image, x, y, rad, color)
+        gfxdraw.filled_circle(image, x, y, rad, color)
+    image.fill(color, rect.inflate(-2*rad,0))
+    image.fill(color, rect.inflate(0,-2*rad))
---
+++ logic/matrices.py
@@ -0,0 +1,11 @@
+def bucketFill(testArray, startX, startY, searchChar='y', fillChar='f'):
+    if testArray[startY][startX]==searchChar:
+        testArray[startY][startX]=fillChar
+        if startY > 0:
+            bucketFill(testArray,startX,startY-1)
+        if startY < len(testArray)-1:
+            bucketFill(testArray,startX,startY+1)
+        if startX > 0:
+            bucketFill(testArray,startX-1,startY)
+        if startX < len(testArray[0])-1:
+            bucketFill(testArray,startX+1,startY)
